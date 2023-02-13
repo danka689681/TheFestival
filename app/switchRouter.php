444f6c801e06c8c816b512fa6e33772b248a9194
Donda <donda@Danielas-MBP.fritz.box>
2023-02-13 23:30:02 +0100

    Admin Environment

--- app/switchRouter.php
+++ app/switchRouter.php
@@ -9,7 +9,26 @@ class SwitchRouter {
                 $controller = new HomeController();
                 $controller->index();
                 break;
-
+            case 'admin': 
+                require __DIR__ . '/Controller/AdminController.php';
+                $controller = new AdminController();
+                $controller->index();
+                break;
+            case 'admin/users': 
+                require __DIR__ . '/Controller/AdminController.php';
+                $controller = new AdminController();
+                $controller->users();
+                break;
+            case 'admin/visitHaarlem': 
+                require __DIR__ . '/Controller/AdminController.php';
+                $controller = new AdminController();
+                $controller->visitHaarlem();
+                break;
+            case 'admin/festival': 
+                require __DIR__ . '/Controller/AdminController.php';
+                $controller = new AdminController();
+                $controller->festival();
+                break;
             default:
                 http_response_code(404);
                 break;
