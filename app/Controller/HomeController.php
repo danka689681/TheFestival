51aadd8b98c47616cf96d9640c017aea6456f3cb
matthiasyanez <683581@student.inholland.nl>
2023-02-26 13:21:39 +0100

    Login logic

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,23 +1,22 @@
 <?php
 // Initialize the session
 require __DIR__ . '/MainController.php';
-require __DIR__ . '/../generalFunctions.php';
 
 
 class HomeController extends Controller {
-    public $navbar;
+    public $header;
     public $footer;
 
     function __construct() {
-        $this->navbar =  __DIR__ . "/../View/header.php"; 
+        $this->header =  __DIR__ . "/../View/header.php"; 
         $this->footer =  __DIR__ . "/../View/footer.php"; 
     }
 
 
     public function index() {
+        require __DIR__ . '/../generalFunctions.php';
         $body = __DIR__ . "/../View/Home/index.php";
-        echo generateContent($this->navbar, $body, $this->footer);
-        require "generatedOutput.php";  
+        eval(' ?>'. generateContent($this->header, $body, $this->footer) .'<?php ');
           
     }
 }
--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -5,57 +5,56 @@ require __DIR__ . '/MainController.php';
 
 class LoginController extends Controller {
     private $UserService;
+    public $header;
+    public $footer;
+
     function __construct() {
         $this->UserService = new UserService();
+        $this->header =  __DIR__ . "/../View/header.php"; 
+        $this->footer =  __DIR__ . "/../View/footer.php"; 
     }
     public function index() {
-
         if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
             header("location: /");
-        }
-
+        } 
         $email = $password = "";
         $email_err = $password_err = $login_err = "";
-
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-            $email = trim($_POST["email"]);
-            $password = trim($_POST["password"]);
-            // Check if email is empty
-            if(empty(trim($_POST["email"]))){
+        // Login
+        if (isset($_POST['login'])) {
+             // Check if email is empty
+            if(empty($_POST["email"])) {
                 $email_err = "Please enter email.";
             } else{
-                $email = trim($_POST["email"]);
+                $email = $_POST["email"];
             }
-            // Check if password is empty
-            if(empty(trim($_POST["password"]))){
+               // Check if password is empty
+            if(empty($_POST["password"])) {
                 $password_err = "Please enter your password.";
-            } else{
-                $password = trim($_POST["password"]);
+            } else {
+                $password = $_POST["password"];
             }
-            // Validate credentials
             if(empty($email_err) && empty($password_err)){
-                $UserService = new UserService();
-                $user = $UserService->getUserByEmail($email);
-
+                $user = $this->UserService->getUserByEmail($email);
+    
                 if(!empty($user)){  
-                        if (password_verify($password, $user->getPassword())) {
-                            session_start();
-                            $_SESSION["loggedin"] = true;
-                            header("location: /");
-
-
-                        } else {
-                            echo 'Invalid password.';
-                        }
-                    
+                    if (password_verify($password, $user->getPassword())) {
+                        // $_SESSION["loggedin"] = true;
+                        header("location: /");
+                    } else {
+                        echo 'Invalid password.';
+                    }    
                 } else {
                     echo 'User does not exist!';
                 }
-
-            // Close statement
-           }
+                // Close statement
+               }
         }
-    require __DIR__ . "/../View/Login/index.php";
+        // Register
+        require __DIR__ . '/../generalFunctions.php';
+        $body = __DIR__ . "/../View/Login/index.php";
+        eval(' ?>'. generateContent($this->header, $body, $this->footer) .'<?php ');
+        
+    }
 
-}
+
 }
